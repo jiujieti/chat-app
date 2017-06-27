9b30b7c958a5c37443847a749955228569c2bf1f
jiujieti <melody2014ymq@gmail.com>
2017-06-27 23:27:57 +0200

    improvements on css

--- index.php
+++ index.php
@@ -47,13 +47,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['type'] == 'login') {
   <div id="container">
     <h1>Let's Chat</h1>
     <form method="POST">
-      <input type="text" name="user"></input>
+      <input type="text" name="user" class="users"></input>
       <input type="hidden" name="type" value="signup"></input>
       <button>signup</button>
     </form>
     <form method="POST">
-      <input type="text" name="sender"></input>
-      <input type="text" name="receiver"></input>
+      <input type="text" name="sender" class="users"></input>
+      <input type="text" name="receiver" class="users"></input>
       <input type="hidden" name="type" value="login"></input>
       <button>login</button>
     </form>
