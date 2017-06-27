28f463b725f42e537b82ef2990a9e01ba465fc60
jiujieti <melody2014ymq@gmail.com>
2017-06-27 22:56:36 +0200

    small improvements

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 require 'include/controllers/userController.php';
 
 // user signup
-if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['user'])) {
+if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['type'] == 'signup') {
   $user = new UserController($_POST['user']);
   if($user->searchUser()) {
     echo "User exists! Please login.";
-  } else { 
+  } else {
+    if(empty($_POST['user'])) {
+      echo "User cannot be empty.";
+    }
     $user->createUser();
     echo "You username is created. Now login and find your friend!";
   }
 }
 
-if($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST['user'])) {
+if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['type'] == 'login') {
   
   $sender = new UserController($_POST['sender']);
   $receiver = new UserController($_POST['receiver']);
@@ -47,11 +50,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST['user'])) {
     <h1>Let's Chat</h1>
     <form method="POST">
       <input type="text" name="user"></input>
+      <input id="signup" name="type" type="hidden" value="signup"></input>
       <button>signup</button>
     </form>
     <form method="POST">
       <input type="text" name="sender"></input>
       <input type="text" name="receiver"></input>
+      <input id="login" name="type" type="hidden" value="login"></input>
       <button>login</button>
     </form>
   </div>
